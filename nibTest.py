69c4677f7b0cd7396f370bf33022a67698d4c7a5
Zenry <57271680+zenyzenyzen@users.noreply.github.com>
2019-12-10 22:21:34 -0500

    Add files via upload

--- nibTest.py
+++ nibTest.py
@@ -2,7 +2,8 @@ import scraper
 import csv
 
 
-def scrape_nutrients(cutoff):  # grabs the nutrient values, cutoff is the point where we stop scraping
+def scrape_nutrients(csvWriter, nib_scrape,
+                     cutoff):  # grabs the nutrient values, cutoff is the point where we stop scraping
     nib_scrape.move_to('data-ingredient-list')
     ingredients = nib_scrape.pull_from_to("=\"", "\"")
 
@@ -75,17 +76,8 @@ def scrape_nutrients(cutoff):  # grabs the nutrient values, cutoff is the point 
                             sugar, protein, diet, allergens, ingredients])
 
 
-#########################
-
-if __name__ == '__main__':
-    nib_scrape = scraper.UrlScraper("https://umassdining.com/locations-menus/worcester/menu",
-                                    headers={'User-Agent': 'Mozilla/5.0'})  # url of current site scraping
-    # Berk URL https://umassdining.com/locations-menus/berkshire
-    # Hamp URL https://umassdining.com/locations-menus/hampshire
-    # Frank URL https://umassdining.com/locations-menus/franklin
-    # Woo URL https://umassdining.com/locations-menus/worcester/menu
-
-    with open('Nibby.csv', 'w+', newline='') as csvfile:
+def build_csv(csv_name, dhall_scraper):  # builds csv line by line,
+    with open(csv_name, 'w+', newline='') as csvfile:
         csvWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csvWriter.writerow(['Name', 'Serving Size', 'Calories', 'Fat Cal', 'Total Fat', 'Daily Value Total Fat',
                             'Saturated Fat', 'Daily Value Saturated Fat', 'Trans Fat', 'Cholesterol',
@@ -93,42 +85,65 @@ if __name__ == '__main__':
                             'Daily Value Total Carbs', 'Fiber', 'Daily Value Fiber', 'Sugar', 'Protein',
                             'Dietary Restrictions', 'Allergens', "Ingredients"])  # headers of the csv
 
-        nib_scrape.move_to('Breakfast Entrees')  # Breakfast
+        dhall_scraper.move_to('Breakfast Entrees')  # Breakfast
         csvWriter.writerow(['Breakfast Entrees'])
         for x in range(200):  # scrapes all the breakfast foods, stops when lunch foods start, 200 just catch all
-            if nib_scrape.scout('lunch_fp') == -1:
+            if dhall_scraper.scout('lunch_fp') == -1:
                 break
             else:
-                scrape_nutrients('lunch_fp')
+                scrape_nutrients(csvWriter, dhall_scraper, 'lunch_fp')
 
-        nib_scrape.move_to('lunch_fp')  # Lunch
+        dhall_scraper.move_to('lunch_fp')  # Lunch
         csvWriter.writerow(['Lunch'])
         for x in range(200):  # scrapes all the lunch foods, stops when dinner foods start, 200 is just big number
-            if nib_scrape.scout('dinner_fp') == -1:
+            if dhall_scraper.scout('dinner_fp') == -1:
                 break
             else:
-                scrape_nutrients('dinner_fp')
+                scrape_nutrients(csvWriter, dhall_scraper, 'dinner_fp')
 
-        nib_scrape.move_to('dinner_fp')  # Dinner
+        dhall_scraper.move_to('dinner_fp')  # Dinner
         csvWriter.writerow(['Dinner'])
         for x in range(200):  # scrapes all the dinner foods, stops when grab and go foods start, 200 is just big number
-            if nib_scrape.scout("Grab' n Go") == -1:
+            if dhall_scraper.scout("Grab' n Go") == -1:
                 break
             else:
-                scrape_nutrients("Grab' n Go")
+                scrape_nutrients(csvWriter, dhall_scraper, "Grab' n Go")
 
-        nib_scrape.move_to("Grab' n Go")  # Grab and Go
+        dhall_scraper.move_to("Grab' n Go")  # Grab and Go
         csvWriter.writerow(["Grab n' Go"])
         for x in range(200):  # scrapes all the grabgo foods, stops when latenight foods start, 200 is just big number
-            if nib_scrape.scout("latenight_fp") == -1:
+            if dhall_scraper.scout("latenight_fp") == -1:
                 break
             else:
-                scrape_nutrients("latenight_fp")
+                scrape_nutrients(csvWriter, dhall_scraper, "latenight_fp")
 
-        nib_scrape.move_to("latenight_fp")  # Late night
+        dhall_scraper.move_to("latenight_fp")  # Late night
         csvWriter.writerow(["Late Night"])
         for x in range(200):  # scrapes all the latenight foods, stops when reachs next menu, 200 is just big number
-            if nib_scrape.scout("Upcoming Menus") == -1:
+            if dhall_scraper.scout("Upcoming Menus") == -1:
                 break
             else:
-                scrape_nutrients("Upcoming Menus")
+                scrape_nutrients(csvWriter, dhall_scraper, "Upcoming Menus")
+
+
+#########################
+
+if __name__ == '__main__':
+    # Berk URL https://umassdining.com/locations-menus/berkshire/menu
+    # Hamp URL https://umassdining.com/locations-menus/hampshire/menu
+    # Frank URL https://umassdining.com/locations-menus/franklin/menu
+    # Woo URL https://umassdining.com/locations-menus/worcester/menu
+
+    berk_scrape = scraper.UrlScraper("https://umassdining.com/locations-menus/berkshire/menu",
+                                     headers={'User-Agent': 'Mozilla/5.0'})
+    hamp_scrape = scraper.UrlScraper("https://umassdining.com/locations-menus/hampshire/menu",
+                                     headers={'User-Agent': 'Mozilla/5.0'})
+    frank_scrape = scraper.UrlScraper("https://umassdining.com/locations-menus/franklin/menu",
+                                      headers={'User-Agent': 'Mozilla/5.0'})
+    woo_scrape = scraper.UrlScraper("https://umassdining.com/locations-menus/worcester/menu",
+                                    headers={'User-Agent': 'Mozilla/5.0'})
+
+    dhalls = {berk_scrape: 'Berk.csv', hamp_scrape: 'Hamp.csv', frank_scrape: 'Frank.csv', woo_scrape: 'Woo.csv'}
+
+    for scrape, csvname in dhalls.items():
+        build_csv(csvname, scrape)
